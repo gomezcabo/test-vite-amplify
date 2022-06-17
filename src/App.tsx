f6a0a4f0f7bb6e591def7c1282199f4938a77396
Juan Gómez <gomezcabo@gmail.com>
2022-06-17 13:25:14 +0100

    Initial commit (setup vite, react, typescript, tailwindcss)

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="tw-p-12">
+      <div className="tw-text-orange-500">App placeholder</div>
+    </div>
+  );
+}
+
+export default App;
