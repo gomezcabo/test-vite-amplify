45d2cf7270d88681ad69d94a1ff940f3825885bb
Juan Gómez <gomezcabo@gmail.com>
2022-06-21 10:06:43 +0100

    Minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,9 @@
 import "./App.css";
 
-import { Amplify } from "aws-amplify";
-import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
-Amplify.configure({
-  Auth: {
-    region: "us-east-1", // REQUIRED - Amazon Cognito Region
-    userPoolId: "us-east-1_zQdJWqzWa", // OPTIONAL - Amazon Cognito User Pool ID
-    userPoolWebClientId: "25tqsnvrfp7tqaruigmoiom907", // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-  },
-});
-
-function Auth() {
-  return (
-    <div className="tw-w-full tw-flex tw-flex-col tw-p-12 tw-items-center">
-      <img
-        className="tw-w-64 tw-mb-12"
-        src="https://dev-cd.energyconnexxion.com/connexxion/static/images/ConnexxionByLynxspringLogo-navbar.png"
-      />
-      <Authenticator />
-    </div>
-  );
-}
-
-function Home() {
+function App() {
   const { user, signOut } = useAuthenticator((context) => [context.user]);
 
   return (
@@ -43,11 +22,6 @@ function Home() {
       </div>
     </main>
   );
-}
-
-function App() {
-  const { authStatus } = useAuthenticator((context) => [context.authStatus]);
-  return authStatus !== "authenticated" ? <Auth /> : <Home />;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,44 @@
-import { Authenticator } from "@aws-amplify/ui-react";
+import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./polyfills";
 
-(window as any).global = window;
+Amplify.configure({
+  Auth: {
+    region: "us-east-1", // REQUIRED - Amazon Cognito Region
+    userPoolId: "us-east-1_zQdJWqzWa", // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolWebClientId: "25tqsnvrfp7tqaruigmoiom907", // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+    cookieStorage: {
+      domain: "localhost", // REQUIRED - Cookie domain (only required if cookieStorage is provided)
+      expires: 0.05,
+      sameSite: "strict", // OPTIONAL - See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
+      secure: true, // OPTIONAL - Cookie secure flag
+    },
+  },
+});
 
-(window as any).process = {
-  env: { DEBUG: undefined },
+const Auth = () => (
+  <div className="tw-w-full tw-flex tw-flex-col tw-p-12 tw-items-center">
+    <img
+      className="tw-w-64 tw-mb-12"
+      src="ConnexxionByLynxspringLogo-navbar.png"
+    />
+    <Authenticator />
+  </div>
+);
+
+const AppMain = () => {
+  const { authStatus } = useAuthenticator((context) => [context.authStatus]);
+  return authStatus !== "authenticated" ? <Auth /> : <App />;
 };
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Authenticator.Provider>
-      <App />
+      <AppMain />
     </Authenticator.Provider>
   </React.StrictMode>
 );
